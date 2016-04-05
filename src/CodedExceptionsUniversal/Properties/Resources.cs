4889fd36890f1ee854d71e058790ff3cafbd74d1
Daniel Kopp <dak@nerdyduck.de>
2016-04-05 14:24:27 +0200

    Fixed severe bug in deployment project.

--- src/CodedExceptionsUniversal/Properties/Resources.cs
+++ src/CodedExceptionsUniversal/Properties/Resources.cs
@@ -19,7 +19,7 @@
  * <assembly name="NerdyDuck.CodedExceptions">
  * Exceptions with custom HRESULTs for .NET
  * </assembly>
- * <file name="Resources.cs" date="2016-04-04">
+ * <file name="Resources.cs" date="2016-04-05">
  * Helper class to access localized string resources.
  * </file>
  ******************************************************************************/
